8d9320f37475c3776e41bd9ae537bfd1440dfa18
Adam Mescher <admescher@gmail.com>
2017-07-13 12:12:40 -0600

    Fixed bookmark box bugs, added ftoggle functionality

--- script.js
+++ script.js
@@ -6,28 +6,63 @@
 //remove .read event
 //delete event
 
-$('.enter-button').on('click', function() {
+
+// Time no longer exists wish-list
+// * 'Are you sure you want to delete this' message when pressing delete button
+
+console.log("page refresh");
+
+var state = {
+  activeBookmarksTotal: 0,
+  activeBookmarksUnread: 0,
+  activeBookmarksRead: 0,
+  totalBookmarksCreated: 0,
+}
+
+$('.enter-button').on('click', function(event) {
   event.preventDefault();
   var bookmarkTitleInput = $('.bookmark-title-input').val();
   var bookmarkUrlInput = $('.bookmark-url-input').val();
 
-  var bookmarkTitle = $('.bookmark-title');
-  var bookmarkUrl = $('.bookmark-url');
+  // var bookmarkTitle = $('.bookmark-title');
+  // var bookmarkUrl = $('.bookmark-url');
 
-  bookmarkTitle.text(bookmarkTitleInput);
-  bookmarkUrl.text(bookmarkUrlInput);
+  createBookmarkBox(bookmarkTitleInput, bookmarkUrlInput);
 
+  state.totalBookmarksCreated += 1;
+
+
+  // if input box === empty disable
+
+  // if something there && valid activate
+
+  // form clear
+  resetForm();
 });
 
-$('.read-link').on('click', function (){
+$('.right-side').on('click', '.read-link', function(event) {
   event.preventDefault();
-  $('.bookmark-box').toggleClass('read-box');
-  $('.bookmark-url-link').toggleClass('read-url');
-  $('.read-link').toggleClass('read-read');
-  $('.delete-link').toggleClass('read-delete');
+  $(this).closest('article').toggleClass('read-box');
+  $(this).parent().parent().siblings().find('.bookmark-url').find('.bookmark-url-link').toggleClass('read-url');
+  $(this).toggleClass('read-read');
+  $(this).closest('div').find('.delete-link').toggleClass('read-delete');
 });
 
-$('.delete-button').on('click', function() {
-  event.preventDefault();
+$('.bookmark-box').on('click', '.delete-button', function() {
   $('.bookmark-box').remove();
-})
+});
+
+function createBookmarkBox(title, url) {
+  $('.right-side').prepend(`<article class="bookmark-box">
+  <h2 class="bookmark-title">${title}</h2>
+  <p class="bookmark-url"><a class="bookmark-url-link" href="${url}">${url}</a></p>
+  <div class="bookmark-bottom-line">
+    <p class="read-button"><span class="read-link">Read</span></p>
+    <p class="delete-button"><span class="delete-link">Delete</span></p>
+  </div>
+  </article>`);
+}
+
+function resetForm() {
+  $('input[type="text"]').val('');
+}
